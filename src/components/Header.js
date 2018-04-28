dc90054b4edc914d14db4b5284c2da8ab7ff6a08
asa <1629520112@qq.com>
2018-04-28 16:52:11 +0800

    modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,39 @@ class Header extends Component {
   constructor(){
     super()
     this.state = {
-      pheight: 0
+      pheight: 0,
+      open: false
     }
+    this.handleResize = this.handleResize.bind(this)
   }
   componentWillMount(){
-    let height = (window.innerHeight - 400)/2
+    this.handleResize()
+    window.addEventListener('resize', this.handleResize)
+  }
+
+  handleMouseOver(e){
+      clearTimeout(this.timer);
+      this.setState({
+        open: true
+      })
+
+  }
+
+  handleMouseOut(e){
+    this.timer = setTimeout(() => {
+      this.setState({
+        open: false
+      })
+    }, 200)
+  }
+  handleResize(e){
+    let height = (window.innerHeight - 308)/2
     this.setState({
       pheight: height
     })
   }
   render(){
+    let openClass = this.state.open ? 'opener active' : 'opener'
     return (
       <div id="header" className="header" style={{"padding-top": this.state.pheight, "padding-bottom": this.state.pheight}}>
         <nav id="nav">
@@ -21,8 +44,22 @@ class Header extends Component {
             <li>
               <a href="javascript:;">Home</a>
             </li>
-            <li>
+            <li className={openClass} onMouseOver={this.handleMouseOver.bind(this)} onMouseOut={this.handleMouseOut.bind(this)}>
               <a href="javascript:;">Dropdown</a>
+              <div className="sub-menu">
+                  <div className="sub-item">
+                    <span>Lorem ipsum dolor</span>
+                  </div>
+                  <div className="sub-item">
+                    <span>Magna phasellus</span>
+                  </div>
+                  <div className="sub-item">
+                    <span>Etiam dolore nisl</span>
+                  </div>
+                  <div className="sub-item">
+                    <span>Veroeros feugiat</span>
+                  </div>
+              </div>
             </li>
             <li>
               <a href="javascript:;">Left Sidebar</a>
